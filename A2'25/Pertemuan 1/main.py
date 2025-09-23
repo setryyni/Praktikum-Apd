4f9db1c60acd990c5c0c4df95125cfca401ee9d9
Setryyni <akiyotempest123@gmail.com>
2025-09-23 16:04:45 +0800

    second commit

---
+++ A2'25/Pertemuan 1/main.py
@@ -0,0 +1,55 @@
+# a = 10
+# b = 3
+# c = a + b
+# print(c)
+
+# a = 0,13
+# b = 0,15
+# c = a+b
+# print (c)
+
+# Nama = 'Harley Baley'
+# hobi = "Makan"
+# print (Nama)
+# print (hobi)
+
+# print ("""Halo,
+#        Saya sedang belajar ngoding
+#        Bahasa Pemograman Python""")
+
+# print ("halo""saya""harley")
+# print ("halo"+"harley")
+
+# text = 'saya suka belajar apd'
+
+# print (text[3])
+# print (text[0:3])
+# print (text[5:])
+# print (text[:8])
+
+# prodi = ['Informatika','sistem informasi', 'Teknik sipil']
+# print (prodi[0])
+# print (prodi[1])
+# print (prodi[2])
+
+# prodi = ['Informatika','sistem informasi', 'Teknik sipil']
+# print (prodi[0:1])
+# print (prodi[1:])
+# print (prodi[:2])
+
+# tuple = ('Harley', 18, True, 'APD')
+# print(tuple[0])
+# print(tuple[2])
+
+# warna = { 
+#     'warna1' : 'merah',
+#     'warna2' : 'kuning',
+#     'warna3' : 'hijau'
+# }
+
+# print(warna['warna1'])
+# print(warna['warna3'])
+
+nama = input ('masukan Nama Anda : ')
+print(nama)
+
